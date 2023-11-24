420f769f7a3efcf7077cd022e08e8417a0b20527
isaac-zarspada <isaaczarspada@gmail.com>
2023-11-23 23:30:40 -0600

    added components

--- main.py
+++ main.py
@@ -7,8 +7,15 @@ clock =  pygame.time.Clock()
 # Setup window
 WIDTH = 1280
 HEIGHT = 960
-scren = pygame.display.set_mode((WIDTH, HEIGHT))
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Pong')
+
+ball = pygame.Rect(WIDTH/2 -15, HEIGHT/2 -15,30,30)
+player = pygame.Rect(WIDTH - 20, HEIGHT/2 - 70,10,140)
+opponent = pygame.Rect(10, HEIGHT/2 - 70,10,140)
+
+bg_color = pygame.Color('grey12')
+light_grey = (200,200,200)
 
 while True:
     # main event loop
@@ -17,7 +24,10 @@ while True:
             pygame.quit()
             sys.exit()
 
+    pygame.draw.rect(screen, light_grey, player)
+    pygame.draw.rect(screen, light_grey, opponent)
+    pygame.draw.ellipse(screen, light_grey, ball)
     
-    # updateing the window
+    # updating the window
     pygame.display.flip()
-    cloc
+    clock.tick(60)
