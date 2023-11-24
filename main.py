15866dc486e5ea785abe3e717c8e1c820f576765
isaac-zarspada <isaaczarspada@gmail.com>
2023-11-23 23:10:35 -0600

    start of pong

---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame, sys
+
+# setup
+pygame.init()
+clock =  pygame.time.Clock()
+
+# Setup window
+WIDTH = 1280
+HEIGHT = 960
+scren = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Pong')
+
+while True:
+    # main event loop
+    for event in pygame.event.get():
+        if event.type == 
