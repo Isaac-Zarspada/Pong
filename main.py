b0b4f9c88162352f13b46b9a2b26f4f592ef9b3c
isaac-zarspada <isaaczarspada@gmail.com>
2023-11-23 23:37:07 -0600

    ball is bouncing

--- main.py
+++ main.py
@@ -17,6 +17,10 @@ opponent = pygame.Rect(10, HEIGHT/2 - 70,10,140)
 bg_color = pygame.Color('grey12')
 light_grey = (200,200,200)
 
+ball_speed_x = 7
+ball_speed_y = 7
+
+
 while True:
     # main event loop
     for event in pygame.event.get():
@@ -24,12 +28,20 @@ while True:
             pygame.quit()
             sys.exit()
 
+    ball.x += ball_speed_x
+    ball.y += ball_speed_y
+
+    if ball.top <= 0 or ball.bottom >= HEIGHT:
+        ball_speed_y *= -1
+    if ball.left <= 0 or ball.right >= WIDTH:
+        ball_speed_x *= -1
+    # visuals
     screen.fill(bg_color)
     pygame.draw.rect(screen, light_grey, player)
     pygame.draw.rect(screen, light_grey, opponent)
     pygame.draw.ellipse(screen, light_grey, ball)
     pygame.draw.aaline(screen,light_grey, (WIDTH/2,0), (WIDTH/2,HEIGHT))
-    
+
     # updating the window
     pygame.display.flip()
     clock.tick(60)
