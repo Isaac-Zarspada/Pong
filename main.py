1e63cc33336fe8b42db2ff47c0f19bf876827aa5
isaac-zarspada <isaaczarspada@gmail.com>
2023-11-23 23:58:30 -0600

    added ball and player functions to organize animat

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 import pygame, sys
 
+def ball_animation():
+    global ball_speed_x, ball_speed_y
+    ball.x += ball_speed_x
+    ball.y += ball_speed_y
+
+    if ball.top <= 0 or ball.bottom >= HEIGHT:
+        ball_speed_y *= -1
+    if ball.left <= 0 or ball.right >= WIDTH:
+        ball_speed_x *= -1
+    if ball.colliderect(player) or ball.colliderect(opponent):
+        ball_speed_x *= -1
+
+def player_animation():
+    player.y += player_speed
+    if player.top <= 0:
+        player.top = 0
+    if player.bottom >= HEIGHT:
+        player.bottom = HEIGHT
+
 # setup
 pygame.init()
 clock =  pygame.time.Clock()
 
 # Setup window
+TICKSPEED = 1
 WIDTH = 1280
 HEIGHT = 960
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -17,24 +37,30 @@ opponent = pygame.Rect(10, HEIGHT/2 - 70,10,140)
 bg_color = pygame.Color('grey12')
 light_grey = (200,200,200)
 
+
 ball_speed_x = 7
 ball_speed_y = 7
-
-
+player_speed = 0
 while True:
     # main event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_DOWN:    
+                player_speed += 7
+            if event.key == pygame.K_UP:    
+                player_speed -= 7
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_DOWN:    
+                player_speed -= 7
+            if event.key == pygame.K_UP:    
+                player_speed += 7
+            
+    ball_animation()
+    player_animation()
 
-    ball.x += ball_speed_x
-    ball.y += ball_speed_y
-
-    if ball.top <= 0 or ball.bottom >= HEIGHT:
-        ball_speed_y *= -1
-    if ball.left <= 0 or ball.right >= WIDTH:
-        ball_speed_x *= -1
     # visuals
     screen.fill(bg_color)
     pygame.draw.rect(screen, light_grey, player)
@@ -44,4 +70,4 @@ while True:
 
     # updating the window
     pygame.display.flip()
-    clock.tick(60)
+    clock.tick(TICKSPEED)
